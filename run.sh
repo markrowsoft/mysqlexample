396c7716d3fc0e16adf17aea3098b0ab15b95264
Mark Engstrom <mark@markrowsoft.com>
2023-11-06 21:58:40 -0500

    added .gitignore

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 docker-compose up -d
-echo "waiting 30 seconds for docker"
-sleep 30
+echo "waiting 10 seconds for docker"
+sleep 10
 source create_table.sh
 python3 -m venv .venv
 python3 -m pip install pip --upgrade
 python3 -m pip install -r requirements.txt
 . ./.venv/bin/activate
+python3 -m pip install mysql-connector-python
+pip freeze > requirements.txt
 python3 saveShow.py
+sleep 5
+# rm -rf ./.venv
+docker-compose down
