29f4ba9f4b359ab9a043bf6e36d9ffd399a57ce8
Mark Engstrom <mark@markrowsoft.com>
2023-11-06 21:40:36 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 docker-compose up -d
-sleep 200
+echo "waiting 30 seconds for docker"
+sleep 30
 source create_table.sh
+python -m venv .venv
+python -m pip install pip --upgrade
+python -m pip install -r requirements.txt
 . ./.venv/bin/activate
 python saveShow.py
