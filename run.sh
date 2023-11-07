c1c6dbbc4cf540cd011ffb8d9ca221a336ef4461
Mark Engstrom <mark@markrowsoft.com>
2023-11-06 22:17:29 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#  uncomment below to install brew
+####################################################
+
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+## install mysql8 client
+# brew mysql-client@8.0
+
+####################################################
+# uncomment below to install docker desktop on MacOS
+####################################################
+# curl -fsSL https://download.docker.com/mac/stable/Docker.dmg -o ~/Docker.dmg
+# hdiutil detach /Volumes/Docker
+# cp -R /Volumes/Docker/Docker.app ~/Applications/
+# hdiutil detach /Volumes/Docker
+# open ~/Applications/Docker.app
+
 docker-compose up -d
 echo "waiting 10 seconds for docker"
 sleep 10
